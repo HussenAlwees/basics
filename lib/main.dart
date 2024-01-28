0f49b4e5a7a79dcd5a4b1ad453fce719325311b2
Hussen <hussain_alwees@ba-tech.tech>
2024-01-28 12:25:52 +0300

    floatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -12,29 +12,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          backgroundColor: Color.fromARGB(255, 32, 119, 205),
+          shape: CircleBorder(eccentricity: 0.5),
+          onPressed: () {
+            print("Hussen");
+          },
+        ), //FloatingActionButton
         appBar: AppBar(
-          title: const Text(" Button :"),
+          title: const Text(" floatingActionButton :"),
           backgroundColor: const Color.fromARGB(255, 175, 206, 232),
         ), //AppBar
         body: Container(
           child: Column(
-            children: [
-              MaterialButton(
-                color: Colors.cyan,
-                textColor: Colors.pink,
-                minWidth: 200,
-                height: 40,
-                padding: EdgeInsets.all(30),
-                onLongPress: () => print("long press on the button "),
-                onPressed: () {
-                  print("the button is pressed");
-                },
-                child: Text(
-                  "Hussen",
-                  style: TextStyle(fontSize: 30.5),
-                ),
-              ),
-            ],
+            children: [],
           ), // column
         ), //Container
       ), //Scaffold
