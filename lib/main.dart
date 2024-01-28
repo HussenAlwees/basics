991b6395c9e842be80e3cb75627f2caf21ec3a9b
Hussen <hussain_alwees@ba-tech.tech>
2024-01-28 14:17:00 +0300

    switch

--- lib/main.dart
+++ lib/main.dart
@@ -12,43 +12,36 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int i = 0;
+  bool switch_state = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(" Statefulwidget :"),
+          title: const Text(" Switch :"),
           backgroundColor: const Color.fromARGB(255, 175, 206, 232),
         ), //AppBar
         body: Container(
+          padding: EdgeInsets.all(10),
           child: Column(
             children: [
-              IconButton(
-                onPressed: () {
-                  setState(() {
-                    i++;
-                  });
-
-                  print(i);
-                },
-                icon: Icon(Icons.add),
-              ),
-              Text("Counter $i"),
-              IconButton(
-                onPressed: () {
-                  setState(() {
-                    i--;
-                  });
-                  print(i);
-                },
-                icon: Icon(Icons.remove),
-              ),
+              Switch(
+                  activeColor: const Color.fromARGB(255, 25, 220, 31),
+                  inactiveThumbColor: const Color.fromARGB(255, 234, 25, 10),
+                  inactiveTrackColor: Colors.orangeAccent,
+                  activeTrackColor: const Color.fromARGB(255, 33, 89, 243),
+                  value: switch_state,
+                  onChanged: (val) {
+                    print("changed");
+                    setState(() {
+                      switch_state = val;
+                    });
+                  }) //switch
             ],
           ), // column
         ), //Container
       ), //Scaffold
-    );
+    ); //MaterialApp
   }
 }
