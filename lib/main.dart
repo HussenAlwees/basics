2e5db9afb8ef3d346663fdb5165e0a7e2ed2d12c
Hussen <hussain_alwees@ba-tech.tech>
2024-01-28 11:47:55 +0300

    Button

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(" Icon button :"),
+          title: const Text(" Button :"),
           backgroundColor: const Color.fromARGB(255, 175, 206, 232),
         ), //AppBar
         body: Container(
-          child: IconButton(
-            onPressed: () {
-              print("icon is pressed");
-            },
-            iconSize: 100.5,
-            icon: const Icon(
-              Icons.person,
-            ), //Icon
-          ), //IconButton
+          child: Column(
+            children: [
+              MaterialButton(
+                color: Colors.cyan,
+                textColor: Colors.pink,
+                minWidth: 200,
+                height: 40,
+                padding: EdgeInsets.all(30),
+                onLongPress: () => print("long press on the button "),
+                onPressed: () {
+                  print("the button is pressed");
+                },
+                child: Text(
+                  "Hussen",
+                  style: TextStyle(fontSize: 30.5),
+                ),
+              ),
+            ],
+          ), // column
         ), //Container
       ), //Scaffold
     ); //MaterialApp
