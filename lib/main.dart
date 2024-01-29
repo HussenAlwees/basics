f621a44f71eb08c40c6cdb615c927869bce53164
Hussen <hussain_alwees@ba-tech.tech>
2024-01-29 21:55:00 +0300

    pageView
    the image don't work i don't know why

--- lib/main.dart
+++ lib/main.dart
@@ -11,39 +11,36 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int selectedindex = 0;
-
-  List<Widget> pages = [
-    Text("Home", style: TextStyle(fontSize: 20.5)),
-    Text("Search", style: TextStyle(fontSize: 20.5)),
-  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("BottomNavigationBar : "),
+          title: Text("pageView : "),
           backgroundColor: const Color.fromARGB(255, 175, 206, 232),
         ), //AppBar
-        bottomNavigationBar: BottomNavigationBar(
-          onTap: (val) {
-            setState(() {
-              selectedindex = val;
-            });
-          },
-          currentIndex: selectedindex,
-          backgroundColor: Colors.blue,
-          selectedItemColor: const Color.fromARGB(255, 3, 118, 7),
-          unselectedItemColor: Colors.black,
-          selectedFontSize: 20,
-          unselectedFontSize: 10,
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-          ],
-        ), //BottomNavigationBar
         body: Container(
-          child: pages.elementAt(selectedindex),
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                child: PageView(
+                  reverse: false,
+                  // physics: NeverScrollableScrollPhysics(),
+                  onPageChanged: (val) {
+                    print(val);
+                  },
+                  children: [
+                    Text("Page1"),
+                    Text("page2"),
+                    // Image.asset("download.jpg", fit: BoxFit.cover),
+                    // Image.asset("flutter_image.jpg", fit: BoxFit.cover),
+                  ],
+                ),
+              )
+            ],
+          ),
         ), //container
       ), //Scaffold
     ); //MaterialApp
