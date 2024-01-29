2667d07cb87ca51b9b9131f951807ca76fbae80b
Hussen <hussain_alwees@ba-tech.tech>
2024-01-29 16:31:01 +0300

    Drower using button

--- lib/main.dart
+++ lib/main.dart
@@ -11,70 +11,29 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Drawer : "),
-          backgroundColor: const Color.fromARGB(255, 175, 206, 232),
-        ), //AppBar
-        drawer: Drawer(
-          backgroundColor: const Color.fromARGB(255, 133, 196, 228),
-          child: Container(
-            padding: EdgeInsets.all(15),
-            child: ListView(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(60),
-                        child: Image.asset(
-                          "images/download.jpg",
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: ListTile(
-                        title: Text("Hussen"),
-                        subtitle: Text("hussen.alwees@gmail.com"),
-                      ),
-                    ),
-                  ],
-                ),
-                ListTile(
-                  title: Text("home"),
-                  leading: Icon(Icons.home),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: Text("About us"),
-                  leading: Icon(Icons.help),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: Text("contact us"),
-                  leading: Icon(Icons.phone),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: Text("signOut"),
-                  leading: Icon(Icons.exit_to_app),
-                  onTap: () {},
-                ),
-              ],
-            ),
-          ),
-        ),
-        endDrawer: Drawer(),
+        key: scaffoldKey,
+        drawer: Drawer(),
         body: Container(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            children: [],
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: MaterialButton(
+                  color: const Color.fromARGB(255, 97, 118, 156),
+                  textColor: Colors.white,
+                  onPressed: () {
+                    scaffoldKey.currentState!.openDrawer();
+                  },
+                  child: Text("Open Drawer"),
+                ),
+              ),
+            ],
           ), //Column
         ), //container
       ), //Scaffold
