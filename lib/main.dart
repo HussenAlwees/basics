86d85359a033478c8be996c8906d5290d4bfca54
Hussen <hussain_alwees@ba-tech.tech>
2024-01-29 19:40:57 +0300

    TabBar with new properties

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,22 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
-        length: 3,
+        length: 2,
+        initialIndex: 0,
         child: Scaffold(
           appBar: AppBar(
             title: Text("TabBar : "),
             backgroundColor: const Color.fromARGB(255, 175, 206, 232),
             bottom: TabBar(
+              indicatorColor: Colors.green,
+              indicatorWeight: 5,
+              labelColor: Color.fromARGB(255, 21, 149, 229),
+              labelStyle: TextStyle(fontSize: 20),
+              unselectedLabelColor: Colors.black,
+              unselectedLabelStyle: TextStyle(fontSize: 10),
               tabs: [
-                Tab(child: Text("Laptops")),
-                Tab(child: Text("Mobiles")),
-                Tab(child: Text("Ipads")),
+                Tab(icon: Icon(Icons.laptop), text: "Laptops"),
+                Tab(icon: Icon(Icons.mobile_friendly), text: "Mobile"),
               ],
             ), //TabBar
           ), //AppBar
@@ -34,7 +40,6 @@ class _MyAppState extends State<MyApp> {
               children: [
                 Text("All Laptops are there"),
                 Text("All Mobiles are there"),
-                Text("All Ipads are there"),
               ],
             ), //TabBarView
           ), //container
