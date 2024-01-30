2b262793416ef9c19a5a9589227b7e1bf226b7c4
Hussen <hussain_alwees@ba-tech.tech>
2024-01-30 11:10:21 +0300

    shortcut
    create a class in a new file.dart and use it in the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_test_project/custom_card.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,36 +12,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List numberOfPages = [1, 2, 3];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("pageView.builder : "),
+          title: Text("shortcut : "),
           backgroundColor: const Color.fromARGB(255, 175, 206, 232),
         ), //AppBar
         body: Container(
           padding: EdgeInsets.all(10),
           child: Column(
             children: [
-              Container(
-                height: 300,
-                child: PageView.builder(
-                  reverse: false,
-                  // physics: NeverScrollableScrollPhysics(),
-                  onPageChanged: (val) {
-                    print(val);
-                  },
-                  itemCount: numberOfPages.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Text(
-                      "Page $index",
-                      style: TextStyle(fontSize: 30),
-                    );
-                  },
-                ),
-              )
+              Custom_List_Tile(
+                  name: 'Hussen',
+                  email: 'hussen.alwees@gamil.com',
+                  date: "2024",
+                  imagename: 'download.jpg'),
+              Custom_List_Tile(
+                  name: 'Ali',
+                  email: 'Ali.al@gamil.com',
+                  date: "2025",
+                  imagename: 'flutter_image.jpg'),
             ],
           ),
         ), //container
