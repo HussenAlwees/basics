9fbc6d4810f3e29c04cdb3eac753f0fee2905bcf
Hussen <hussain_alwees@ba-tech.tech>
2024-02-01 15:18:23 +0300

    example for Ecommerce
    in this code, we write many containers for every component, the next code we will make it more  Optimized and organized by make List

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,34 +4,246 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home page : "),
-        backgroundColor: const Color.fromARGB(255, 175, 206, 232),
-      ),
-      body: ListView(
-        children: [
-          Center(
-            child: MaterialButton(
-              color: Colors.green,
-              textColor: Colors.white,
-              child: const Text("get the Image"),
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text("Image is loading"),
-                    duration: Duration(seconds: 10),
-                    backgroundColor: Colors.green,
-                    action: SnackBarAction(
-                        label: "OK",
-                        onPressed: () {
-                          print("pressed");
-                        }),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                      prefixIcon: Icon(Icons.search),
+                      hintText: "Search",
+                    ),
                   ),
-                );
-              },
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Icon(
+                    Icons.menu,
+                    size: 40,
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+            Container(
+              height: 30,
+            ),
+            Text(
+              "Categories",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              height: 20,
+            ),
+            Container(
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(100)),
+                        child: Icon(
+                          Icons.laptop,
+                          size: 40,
+                        ),
+                        padding: EdgeInsets.all(15),
+                      ),
+                      Text(
+                        "Laptops",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey[800]),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(100)),
+                        child: Icon(
+                          Icons.mobile_friendly,
+                          size: 40,
+                        ),
+                        padding: EdgeInsets.all(15),
+                      ),
+                      Text(
+                        "Mobile",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey[800]),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(100)),
+                        child: Icon(
+                          Icons.electric_bike,
+                          size: 40,
+                        ),
+                        padding: EdgeInsets.all(15),
+                      ),
+                      Text(
+                        "Elecric bike",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey[800]),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(100)),
+                        child: Icon(
+                          Icons.camera_alt_outlined,
+                          size: 40,
+                        ),
+                        padding: EdgeInsets.all(15),
+                      ),
+                      Text(
+                        "Cameras",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey[800]),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(100)),
+                        child: Icon(
+                          Icons.electric_car,
+                          size: 40,
+                        ),
+                        padding: EdgeInsets.all(15),
+                      ),
+                      Text(
+                        "Cars",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey[800]),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                "Best Selling",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            GridView(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, mainAxisExtent: 230),
+              children: [
+                Card(
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 250,
+                        padding: EdgeInsets.all(20),
+                        color: Colors.grey[200],
+                        child: Image.asset(
+                          "images/1.png",
+                          height: 100,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      Text(
+                        "car1",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                      Text(
+                        "description : faster car",
+                        style: TextStyle(fontSize: 14, color: Colors.grey[800]),
+                      ),
+                      Text(
+                        "500\$",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange),
+                      ),
+                    ],
+                  ),
+                ),
+                Card(
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 250,
+                        padding: EdgeInsets.all(20),
+                        color: Colors.grey[200],
+                        child: Image.asset(
+                          "images/2.png",
+                          height: 100,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      Text(
+                        "car2",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                      Text(
+                        "description : big car",
+                        style: TextStyle(fontSize: 14, color: Colors.grey[800]),
+                      ),
+                      Text(
+                        "550\$",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
