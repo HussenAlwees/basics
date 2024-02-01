46b9ab14feb8067974e60091c20d4752c9b18500
Hussen <hussain_alwees@ba-tech.tech>
2024-02-01 15:43:29 +0300

    complete the example for Ecommerce
    here we define two Lists to make our code tidy

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class Homepage extends StatelessWidget {
+  List<Map> categories = [
+    {'title': 'Laptops', 'iconName': Icons.laptop},
+    {'title': 'Mobiles', 'iconName': Icons.mobile_friendly},
+    {'title': 'Electric bikes', 'iconName': Icons.electric_bike},
+    {'title': 'Cameras', 'iconName': Icons.camera_alt_outlined},
+    {'title': 'Cars', 'iconName': Icons.electric_car},
+  ];
+
+  List<Map> items = [
+    {
+      'image': "images/1.png",
+      'title': "car1",
+      'subtitle': "description : faster car",
+      'price': "500\$"
+    },
+    {
+      'image': "images/2.png",
+      'title': "car2",
+      'subtitle': "description : big car",
+      'price': "550\$"
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,119 +67,38 @@ class Homepage extends StatelessWidget {
             Container(
               height: 20,
             ),
+
+            //****************** for categories ************************//
             Container(
               height: 100,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(right: 10),
-                        decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.circular(100)),
-                        child: Icon(
-                          Icons.laptop,
-                          size: 40,
+              child: ListView.builder(
+                  itemCount: categories.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, i) {
+                    return Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 10),
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(100)),
+                          child: Icon(
+                            categories[i]['iconName'],
+                            size: 40,
+                          ),
+                          padding: EdgeInsets.all(15),
                         ),
-                        padding: EdgeInsets.all(15),
-                      ),
-                      Text(
-                        "Laptops",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey[800]),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(right: 10),
-                        decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.circular(100)),
-                        child: Icon(
-                          Icons.mobile_friendly,
-                          size: 40,
-                        ),
-                        padding: EdgeInsets.all(15),
-                      ),
-                      Text(
-                        "Mobile",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey[800]),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(right: 10),
-                        decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.circular(100)),
-                        child: Icon(
-                          Icons.electric_bike,
-                          size: 40,
-                        ),
-                        padding: EdgeInsets.all(15),
-                      ),
-                      Text(
-                        "Elecric bike",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey[800]),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(right: 10),
-                        decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.circular(100)),
-                        child: Icon(
-                          Icons.camera_alt_outlined,
-                          size: 40,
-                        ),
-                        padding: EdgeInsets.all(15),
-                      ),
-                      Text(
-                        "Cameras",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey[800]),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(right: 10),
-                        decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.circular(100)),
-                        child: Icon(
-                          Icons.electric_car,
-                          size: 40,
-                        ),
-                        padding: EdgeInsets.all(15),
-                      ),
-                      Text(
-                        "Cars",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey[800]),
-                      )
-                    ],
-                  ),
-                ],
-              ),
+                        Text(
+                          categories[i]['title'],
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey[800]),
+                        )
+                      ],
+                    );
+                  }),
             ),
+            /////////////////////////
             Container(
               margin: EdgeInsets.symmetric(vertical: 10),
               child: Text(
@@ -166,13 +109,15 @@ class Homepage extends StatelessWidget {
                 ),
               ),
             ),
-            GridView(
+            //****************** for Best Selling ************************//
+            GridView.builder(
+              itemCount: items.length,
               physics: NeverScrollableScrollPhysics(),
               shrinkWrap: true,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2, mainAxisExtent: 230),
-              children: [
-                Card(
+              itemBuilder: (context, index) {
+                return Card(
                   child: Column(
                     children: [
                       Container(
@@ -180,24 +125,24 @@ class Homepage extends StatelessWidget {
                         padding: EdgeInsets.all(20),
                         color: Colors.grey[200],
                         child: Image.asset(
-                          "images/1.png",
+                          items[index]['image'],
                           height: 100,
                           fit: BoxFit.fill,
                         ),
                       ),
                       Text(
-                        "car1",
+                        items[index]['title'],
                         style: TextStyle(
                             fontSize: 20,
                             fontWeight: FontWeight.bold,
                             color: Colors.black),
                       ),
                       Text(
-                        "description : faster car",
+                        items[index]['subtitle'],
                         style: TextStyle(fontSize: 14, color: Colors.grey[800]),
                       ),
                       Text(
-                        "500\$",
+                        items[index]['price'],
                         style: TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.bold,
@@ -205,42 +150,8 @@ class Homepage extends StatelessWidget {
                       ),
                     ],
                   ),
-                ),
-                Card(
-                  child: Column(
-                    children: [
-                      Container(
-                        width: 250,
-                        padding: EdgeInsets.all(20),
-                        color: Colors.grey[200],
-                        child: Image.asset(
-                          "images/2.png",
-                          height: 100,
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                      Text(
-                        "car2",
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                      Text(
-                        "description : big car",
-                        style: TextStyle(fontSize: 14, color: Colors.grey[800]),
-                      ),
-                      Text(
-                        "550\$",
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.orange),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                );
+              },
             ),
           ],
         ),
