ae61558c573398d371f4dfd1b9d9732cef30d04c
Hussen <hussain_alwees@ba-tech.tech>
2024-02-04 15:09:37 +0300

    SharedPreferences (we use this package to save the variables in the mobile memory, so even if we close the app the variable still has his value.)

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -13,7 +13,7 @@ class _Homepage extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("HTTP API : "),
+        title: Text("SharedPreferences : "),
         backgroundColor: const Color.fromARGB(255, 175, 206, 232),
       ),
       body: ListView(
