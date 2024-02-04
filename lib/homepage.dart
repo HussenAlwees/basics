318fc2bfb97ca85fe9f83be3819b8b9dad7eb627
Hussen <hussain_alwees@ba-tech.tech>
2024-02-04 17:55:10 +0300

    theme

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,14 +12,28 @@ class _Homepage extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Text Font : "),
+        title: const Text("theme : "),
         backgroundColor: const Color.fromARGB(255, 175, 206, 232),
       ),
       body: ListView(
         children: [
-          Text(
-            "New Font",
-            style: TextStyle(fontFamily: "ProtestRevolution", fontSize: 30),
+          Center(
+            child: Column(
+              children: [
+                Text(
+                  "bodySmall theme",
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                Text(
+                  "bodyLarge theme",
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                Text(
+                  "bodyMedium theme",
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ],
+            ),
           ),
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,26 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "ProtestRevolution",
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
+        ),
+        textTheme: TextTheme(
+          bodySmall: TextStyle(
+              color: Color.fromARGB(255, 96, 226, 61),
+              fontSize: 16,
+              fontWeight: FontWeight.bold),
+          bodyLarge: TextStyle(
+              color: Color.fromARGB(255, 229, 84, 84),
+              fontSize: 25,
+              fontWeight: FontWeight.bold),
+          bodyMedium: TextStyle(
+              color: Color.fromARGB(255, 41, 150, 205),
+              fontSize: 20,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
       home: Homepage(),
     ); //MaterialApp
   }
