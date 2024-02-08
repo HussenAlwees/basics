a62eeed1219522cf1e6ad207e0c8dff0e9ad9047
Hussen <hussain_alwees@ba-tech.tech>
2024-02-08 12:38:30 +0300

    Dialog using GetX

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,17 +1,15 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
-import 'package:new_test_project/settingsservices.dart';
 
 // ignore: must_be_immutable
-class Home extends GetView<SettingsServices> {
+class Home extends StatelessWidget {
   Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // SettingsServices c=Get.find();
     return Scaffold(
       appBar: AppBar(
-        title: Text("1".tr),
+        title: Text("Dialog"),
         backgroundColor: const Color.fromARGB(255, 175, 206, 232),
       ),
       body: Container(
@@ -19,27 +17,55 @@ class Home extends GetView<SettingsServices> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GetX<SettingsServices>(
-              builder: (c) => Center(
-                child: Text("${c.counter}"),
+            Center(
+              child: MaterialButton(
+                color: Colors.green,
+                onPressed: () {
+                  Get.defaultDialog(
+                      title: "Dialog Title",
+                      middleText: "This is a dialog",
+                      textCancel: "Cancel",
+                      textConfirm: "OK",
+                      onConfirm: () {
+                        print("Ok");
+                      },
+                      onCancel: () {
+                        Get.back();
+                      });
+                },
+                child: Text("Show first Dialog"),
               ),
+            ),
+            Container(
+              height: 30,
             ),
             Center(
               child: MaterialButton(
                 color: Colors.green,
                 onPressed: () {
-                  controller.increase();
+                  Get.defaultDialog(
+                      title: "Dialog Title",
+                      content: Column(
+                        children: [
+                          Text("Enter user name:"),
+                          TextFormField(),
+                        ],
+                      ),
+                      titleStyle: TextStyle(color: Colors.blue, fontSize: 16),
+                      actions: [
+                        MaterialButton(
+                          onPressed: () {},
+                          child: Text("ok"),
+                        ),
+                        MaterialButton(
+                          onPressed: () {
+                            Get.back();
+                          },
+                          child: Text("cancel"),
+                        ),
+                      ]);
                 },
-                child: Text("2".tr),
-              ),
-            ),
-            Center(
-              child: MaterialButton(
-                color: Colors.green,
-                onPressed: () {
-                  controller.shardprefs.clear();
-                },
-                child: Text("3".tr),
+                child: Text("Show second Dialog"),
               ),
             ),
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:new_test_project/homepage.dart';
-import 'package:new_test_project/local_language/locale.dart';
-import 'package:new_test_project/settingsservices.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initalServices();
+void main() {
   runApp(MyApp());
-}
-
-Future initalServices() async {
-  await Get.putAsync(() => SettingsServices().init());
 }
 
 class MyApp extends StatelessWidget {
@@ -22,8 +14,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      locale: Get.deviceLocale,
-      translations: MyLocale(),
       initialRoute: "/home",
       getPages: [
         GetPage(name: "/home", page: () => Home()),
