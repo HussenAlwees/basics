f40a00329a53669d675eda0aeca49622bb5b22bf
Hussen <hussain_alwees@ba-tech.tech>
2024-02-26 11:03:51 +0300

    changeNotifierProvider and consumer

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,59 @@
 // main.dart
 import 'package:flutter/material.dart';
-import 'package:async/async.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TestOne());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class TestOne extends StatefulWidget {
+  const TestOne({super.key});
+
+  @override
+  State<TestOne> createState() => _TestOneState();
+}
+
+class _TestOneState extends State<TestOne> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        // Remove the debug banner
-        debugShowCheckedModeBanner: false,
-        title: 'KindaCode.com',
-        theme: ThemeData(
-          primarySwatch: Colors.red,
+    return ChangeNotifierProvider(
+      create: (context) => Model(),
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Provider'),
+            backgroundColor: const Color.fromARGB(255, 175, 206, 232),
+          ),
+          body: Consumer<Model>(builder: (context, value, child) {
+            return Column(
+              children: [
+                Center(
+                  child: Text("${value.text}"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                MaterialButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text("change the text"),
+                  onPressed: () {
+                    value.changetext();
+                  },
+                ),
+              ],
+            );
+          }),
         ),
-        home: const HomePage());
+      ),
+    );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  // this future will return some text once it completes
-  Future<String?> _myFuture() async {
-    print("myfuture1");
-    await Future.delayed(const Duration(seconds: 5));
-    print("myfuture2");
-    return 'Future completed';
-  }
-
-  // keep a reference to CancelableOperation
-  CancelableOperation? _myCancelableFuture;
-
-  // This is the result returned by the future
-  String? _text;
-
-  // Help you know whether the app is "loading" or not
-  bool _isLoading = false;
-
-  // This function is called when the "start" button is pressed
-  void _getData() async {
-    setState(() {
-      _isLoading = true;
-    });
-
-    _myCancelableFuture = CancelableOperation.fromFuture(
-      _myFuture(),
-      onCancel: () {
-        print("cancel");
-        return 'Future has been canceld';
-      },
-    );
-    final value = await _myCancelableFuture?.value;
-    print("object");
-    // update the UI
-    setState(() {
-      _text = value;
-      _isLoading = false;
-    });
-  }
-
-  // this function is called when the "cancel" button is tapped
-  void _cancelFuture() async {
-    final result = await _myCancelableFuture?.cancel();
-    print("result= $result");
-    setState(() {
-      _text = result;
-      _isLoading = false;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('KindaCode.com')),
-      body: Center(
-        child: _isLoading
-            ? const CircularProgressIndicator()
-            : Text(
-                _text ?? 'Press Start Button',
-                style: const TextStyle(fontSize: 28),
-              ),
-      ),
-      // This button is used to trigger _getDate() and _cancelFuture() functions
-      // the function is called depends on the _isLoading variable
-      floatingActionButton: ElevatedButton(
-        onPressed: () => _isLoading ? _cancelFuture() : _getData(),
-        style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30), backgroundColor: _isLoading ? Colors.red : Colors.indigo),
-        child: Text(_isLoading ? 'Cancel' : 'Start'),
-      ),
-    );
+class Model extends ChangeNotifier {
+  String text = "Hello";
+  changetext() {
+    text = "Hello World";
+    notifyListeners();
   }
 }
