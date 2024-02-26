2aebda6adf9055a776cbcff42727aebbed4b2c63
Hussen <hussain_alwees@ba-tech.tech>
2024-02-26 09:36:21 +0300

    CancelableOperation

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,103 @@
+// main.dart
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:new_test_project/homepage.dart';
-import 'package:new_test_project/newpage.dart';
+import 'package:async/async.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: "main page : ",
-      theme: theme.lightTheme,
-      initialRoute: "/home",
-      getPages: [
-        GetPage(name: "/home", page: () => Home()),
-        GetPage(name: "/newPage", page: () => newpage()),
-      ],
-    ); //MaterialApp
+    return MaterialApp(
+        // Remove the debug banner
+        debugShowCheckedModeBanner: false,
+        title: 'KindaCode.com',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: const HomePage());
   }
 }
 
-class theme {
-  static ThemeData lightTheme =
-      ThemeData.light().copyWith(appBarTheme: AppBarTheme(color: Colors.red));
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  static ThemeData darkTheme =
-      ThemeData.dark().copyWith(appBarTheme: AppBarTheme(color: Colors.pink));
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // this future will return some text once it completes
+  Future<String?> _myFuture() async {
+    print("myfuture1");
+    await Future.delayed(const Duration(seconds: 5));
+    print("myfuture2");
+    return 'Future completed';
+  }
+
+  // keep a reference to CancelableOperation
+  CancelableOperation? _myCancelableFuture;
+
+  // This is the result returned by the future
+  String? _text;
+
+  // Help you know whether the app is "loading" or not
+  bool _isLoading = false;
+
+  // This function is called when the "start" button is pressed
+  void _getData() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    _myCancelableFuture = CancelableOperation.fromFuture(
+      _myFuture(),
+      onCancel: () {
+        print("cancel");
+        return 'Future has been canceld';
+      },
+    );
+    final value = await _myCancelableFuture?.value;
+    print("object");
+    // update the UI
+    setState(() {
+      _text = value;
+      _isLoading = false;
+    });
+  }
+
+  // this function is called when the "cancel" button is tapped
+  void _cancelFuture() async {
+    final result = await _myCancelableFuture?.cancel();
+    print("result= $result");
+    setState(() {
+      _text = result;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('KindaCode.com')),
+      body: Center(
+        child: _isLoading
+            ? const CircularProgressIndicator()
+            : Text(
+                _text ?? 'Press Start Button',
+                style: const TextStyle(fontSize: 28),
+              ),
+      ),
+      // This button is used to trigger _getDate() and _cancelFuture() functions
+      // the function is called depends on the _isLoading variable
+      floatingActionButton: ElevatedButton(
+        onPressed: () => _isLoading ? _cancelFuture() : _getData(),
+        style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30), backgroundColor: _isLoading ? Colors.red : Colors.indigo),
+        child: Text(_isLoading ? 'Cancel' : 'Start'),
+      ),
+    );
+  }
 }
